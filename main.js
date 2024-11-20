2a44ed6bec1ab9dc833963709f2c26b26bb40e70
黃映翔 <aa0955059329@gmail.com>
2024-11-20 15:10:41 +0800

    week6

--- main.js
+++ main.js
@@ -1,40 +1,50 @@
 import './assets/scss/all.scss';
+// const data = [
+//     {
+//         name: "綠島自由行套裝行程",
+//         pic: "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_1.png?raw=true",
+//         city: "高雄",
+//         price: 1400,
+//         num: 87,
+//         star: 10,
+//         introduce: "嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合。"
+//     },
+//     {
+//         name: "清境高空觀景步道",
+//         pic: "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_4.png?raw=true",
+//         city: "台北",
+//         price: 240,
+//         num: 99,
+//         star: 2,
+//         introduce: "清境農場青青草原數十公頃碧草，這些景觀豐沛了清境觀景步道的風格，也涵養它無可取代的特色。"
+//     },
+//     {
+//         name: "山林悠遊套票",
+//         pic: "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_3.png?raw=true",
+//         city: "台中",
+//         price: 1765,
+//         num: 20,
+//         star: 7,
+//         introduce: "山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點。"
+//     }
+// ]
 
-
-const data = [
-    {
-        name : "綠島自由行套裝行程",
-        pic: "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_1.png?raw=true",
-        city : "高雄",
-        price : 1400,
-        num: 87,
-        star: 10,
-        introduce: "嚴選超高CP值綠島自由行套裝行程，多種綠島套裝組合。"
-    },
-    {
-        name: "清境高空觀景步道",
-        pic: "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_4.png?raw=true",
-        city: "台北",
-        price: 240,
-        num: 99,
-        star: 2,
-        introduce: "清境農場青青草原數十公頃碧草，這些景觀豐沛了清境觀景步道的風格，也涵養它無可取代的特色。"
-    },
-    {
-        name: "山林悠遊套票",
-        pic: "https://github.com/hexschool/2022-web-layout-training/blob/main/js_week5/travel_3.png?raw=true",
-        city:"台中",
-        price: 1765,
-        num: 20,
-        star: 7,
-        introduce: "山林悠遊套票，結合南投清境高空步道、雙龍瀑布七彩吊橋、瑞龍瀑布園區之熱門景點。"
-    }
-]
+//JSON 提取data
+let data 
+axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+    .then(response => {
+      data = response.data.data
+      searchResultText.querySelector("span").innerHTML = data.length
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
 
 const regionSearch = document.querySelector(".regionSearch"),
       ticketCardArea = document.querySelector(".ticketCard-area"),
-    addTicketForm = document.querySelector(".addTicket-form")
-
+      addTicketForm = document.querySelector(".addTicket-form"),
+      searchResultText = document.querySelector("#searchResult-text")
+         
 //data init
 function dataInit() {
     ticketCardArea.innerHTML = ""
@@ -42,10 +52,10 @@ function dataInit() {
         ticketCardArea.innerHTML += `<li class="ticketCard">
           <div class="ticketCard-img">
             <a href="#">
-              <img src=${e.pic} alt="">
+              <img src=${e.imgUrl} alt="">
             </a>
-            <div class="ticketCard-region">${e.city}</div>
-            <div class="ticketCard-rank">${e.star}</div>
+            <div class="ticketCard-region">${e.area}</div>
+            <div class="ticketCard-rank">${e.rate}</div>
           </div>
           <div class="ticketCard-content">
             <div>
@@ -53,16 +63,16 @@ function dataInit() {
                 <a href="#" class="ticketCard-name">${e.name}</a>
               </h3>
               <p class="ticketCard-description">
-               ${e.introduce}
+               ${e.description}
               </p>
             </div>
             <div class="ticketCard-info">
               <p class="ticketCard-num">
                 <span><i class="fas fa-exclamation-circle"></i></span>
-                剩下最後 <span id="ticketCard-num">  ${e.num} </span> 組
+                剩下最後 <span id="ticketCard-num">  ${e.group} </span> 組
               </p>
               <p class="ticketCard-price">
-                TWD <span id="ticketCard-price">${e.price}</span>
+                TWD <span id="ticketCard-price">$${e.price}</span>
               </p>
             </div>
           </div>
@@ -71,33 +81,42 @@ function dataInit() {
 }
 
 //新增資料
-addTicketForm.addEventListener("submit", event=>{   
+addTicketForm.addEventListener("submit", event => {
     event.preventDefault();
     const obj = {
         name: event.target.ticketName.value,
-        pic: event.target.ticketImgUrl.value,
-        city: event.target.ticketRegion.value,
+        imgUrl: event.target.ticketImgUrl.value,
+        area: event.target.ticketRegion.value,
         price: event.target.ticketPrice.value,
-        num: event.target.ticketNum.value,
-        star: event.target.ticketRate.value,
-        introduce: event.target.ticketDescription.value
+        group: event.target.ticketNum.value,
+        rate: event.target.ticketRate.value,
+        description: event.target.ticketDescription.value
     }
-    if (obj.name == "" || obj.pic == "" || obj.city == "" || obj.price == "" || obj.num == "" || obj.star == "" || obj.introduce == ""){
+    if (Object.values(obj).some(value => value === "")) {
         alert("未填寫完成")
         return
     }
+    console.log(data)
     data.push(obj)
+    searchResultText.querySelector("span").innerHTML = data.length
     dataInit()
     addTicketForm.reset()
 })
 
 //地區篩選器
-regionSearch.addEventListener("change",event=>{
+regionSearch.addEventListener("change", event => {
     const ticketCards = document.querySelectorAll(".ticketCard"),
           selectRegion = event.target.value
-    ticketCards.forEach((iteam)=>{
+    let n = 0
+    ticketCards.forEach((iteam) => {
         const region = iteam.querySelector(".ticketCard-region").innerHTML
-        iteam.style.display = (selectRegion == "" || selectRegion == region) ? "block" : "none"
+        if (selectRegion == "" || selectRegion == region) {
+            iteam.style.display ="block"
+            n++
+        } else {
+            iteam.style.display ="none"
+        }
+        searchResultText.querySelector("span").innerHTML = n
     })
 })
 
